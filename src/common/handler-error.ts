9322b4805c93d13c1f6b0319c490050fee1f529d
James Watkins-Harvey <james.watkinsharvey@temporal.io>
2025-07-15 04:19:19 -0400

    chore: Improve HandlerErrorType: const obj of strings, and docs

--- src/common/handler-error.ts
+++ src/common/handler-error.ts
@@ -2,43 +2,72 @@ import { injectSymbolBasedInstanceOf } from "../internal/symbol-instanceof";
 
 /**
  * An error type associated with a {@link HandlerError}, defined according to the Nexus specification.
- * Only the types defined as consts in this package are valid. Do not use other values. See each type's details below:
  *
- * BAD_REQUEST - The server cannot or will not process the request due to an apparent client error. Clients should not
- * retry this request unless advised otherwise.
- *
- * UNAUTHENTICATED - The client did not supply valid authentication credentials for this request. Clients should not
- * retry this request unless advised otherwise.
- *
- * UNAUTHORIZED - The caller does not have permission to execute the specified operation. Clients should not retry this
- * request unless advised otherwise.
- *
- * NOT_FOUND - The requested resource could not be found but may be available in the future. Clients should not retry
- * this request unless advised otherwise.
- *
- * RESOURCE_EXHAUSTED - Some resource has been exhausted, perhaps a per-user quota, or perhaps the entire file system
- * is out of space. Subsequent requests by the client are permissible.
- *
- * INTERNAL - An internal error occured. Subsequent requests by the client are permissible.
- *
- * NOT_IMPLEMENTED - The server either does not recognize the request method, or it lacks the ability to fulfill the
- * request. Clients should not retry this request unless advised otherwise.
- *
- * UNAVAILABLE - The service is currently unavailable. Subsequent requests by the client are permissible.
- *
- * UPSTREAM_TIMEOUT - Used by gateways to report that a request to an upstream server has timed out. Subsequent
- * requests by the client are permissible.
+ * @experimental
  */
-export type HandlerErrorType =
-  | "BAD_REQUEST"
-  | "UNAUTHENTICATED"
-  | "UNAUTHORIZED"
-  | "NOT_FOUND"
-  | "RESOURCE_EXHAUSTED"
-  | "INTERNAL"
-  | "NOT_IMPLEMENTED"
-  | "UNAVAILABLE"
-  | "UPSTREAM_TIMEOUT";
+export type HandlerErrorType = (typeof HandlerErrorType)[keyof typeof HandlerErrorType];
+export const HandlerErrorType = {
+  /**
+   * The handler cannot or will not process the request due to an apparent client error.
+   *
+   * Clients should not retry this request unless advised otherwise.
+   */
+  BAD_REQUEST: "BAD_REQUEST",
+
+  /**
+   * The client did not supply valid authentication credentials for this request.
+   *
+   * Clients should not retry this request unless advised otherwise.
+   */
+  UNAUTHENTICATED: "UNAUTHENTICATED",
+
+  /**
+   * The caller does not have permission to execute the specified operation.
+   *
+   * Clients should not retry this request unless advised otherwise.
+   */
+  UNAUTHORIZED: "UNAUTHORIZED",
+
+  /**
+   * The requested resource could not be found but may be available in the future.
+   */
+  NOT_FOUND: "NOT_FOUND",
+
+  /**
+   * Some resource has been exhausted, perhaps a per-user quota, or perhaps the entire file system
+   * is out of space.
+   *
+   * Subsequent requests by the client are permissible.
+   */
+  RESOURCE_EXHAUSTED: "RESOURCE_EXHAUSTED",
+
+  /**
+   * An internal error occured.
+   *
+   * Subsequent requests by the client are permissible.
+   */
+  INTERNAL: "INTERNAL",
+
+  /**
+   * The server either does not recognize the request method, or it lacks the ability to fulfill the
+ * request. Clients should not retry this request unless advised otherwise.
+   */
+  NOT_IMPLEMENTED: "NOT_IMPLEMENTED",
+
+  /**
+   * The service is currently unavailable.
+   *
+   * Subsequent requests by the client are permissible.
+   */
+  UNAVAILABLE: "UNAVAILABLE",
+
+  /**
+   * Used by gateways to report that a request to an upstream server has timed out.
+   *
+   * Subsequent requests by the client are permissible.
+ */
+  UPSTREAM_TIMEOUT: "UPSTREAM_TIMEOUT",
+} as const;
 
 /**
  * Options for constructing a {@link HandlerError} from a message, type, and a retryable flag.
