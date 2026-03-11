0c66bfdd3ffec36a84a7b9211f5048cde00d9c77
Alex Mazzeo <alex.mazzeo@temporal.io>
2026-03-11 13:49:00 -0700

    Run formatter

--- src/common/handler-error.ts
+++ src/common/handler-error.ts
@@ -117,7 +117,9 @@ export class HandlerError extends Error {
     options?: HandlerErrorOptions,
   ): HandlerError {
     const resolvedType =
-      type in HandlerErrorType ? HandlerErrorType[type as keyof typeof HandlerErrorType] : HandlerErrorType.UNKNOWN;
+      type in HandlerErrorType
+        ? HandlerErrorType[type as keyof typeof HandlerErrorType]
+        : HandlerErrorType.UNKNOWN;
 
     return new HandlerError(resolvedType, message, {
       ...options,
